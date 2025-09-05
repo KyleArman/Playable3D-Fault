9eb308f2ee5769605dd698fb0101267fe4061b6e
KyleArman <muhamad.haikal@graduate.utm.my>
2025-09-05 12:21:49 +0800

    Intial Deploy
    Initial Deploy

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "KhairulIzzul-ARAircondSimulation-1.0";
+const contentToCache = [
+    "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
+    "Build/f82e37022a8cf23bab8773977413b012.framework.js",
+    "Build/653e629a5da7c14b2125bba06ecb5417.data",
+    "Build/2cb3125d77bc12770267622e67f52a5e.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
